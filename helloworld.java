156e061f0c7b4ce849e760e073422c9c44ab5afc
komal Aujla <komalaujla2019@gmail.com>
2019-12-17 11:21:32 +0000

    added new statement

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 //this is new file
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
+      // Prints "Hello, World this is komal aujla" in the terminal window.
       System.out.println("Hello, World");
    }
 }
